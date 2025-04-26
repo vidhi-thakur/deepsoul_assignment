b670605ac065de47316571d36280341ec9176eb3
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2025-04-27 01:51:00 +0530

    css setup for theme toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
+    darkMode: 'class',
     theme: {
-        extend: {
-            colors: {
-                background: "#fdfdfd",
-                foreground: "#111111",
-                border: "#e5e7eb",
-            }
-        }
-    }
+        extend: {}
+    },
 }
